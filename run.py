509f250f447d3b6f8e54cc86ef2d2cfa5f47d3f1
Mae <laprestm@oregonstate.edu>
2020-06-19 00:05:42 -0500

    Added fileupload form and file save route in flask

--- run.py
+++ run.py
@@ -1,13 +1,48 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER='./static/audio'
+ALLOWED_EXTENSIONS = {'wav', 'mp4', 'avi'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'6\x9eL~l\xbb2\xed\xe0&\x08\xbf\xa4@\xc2\x9e'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    return render_template("index.html")
+    if request.method == 'POST':
+        print(request)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print(file.filename)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('processFile',
+                                    filename=filename))
+    else:
+        return render_template("upload.html")
+
+@app.route('/processFile')
+def process_file(filename):
+    return "file added"
+
+
 
 @app.errorhandler(404)
 def not_found(error):

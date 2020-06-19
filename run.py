169ece43b23e97b5e0bd24e0b4c6848ad18670c5
Mae <laprestm@oregonstate.edu>
2020-06-18 21:54:41 -0500

    Error handlers

--- run.py
+++ run.py
@@ -8,3 +8,11 @@ def index():
 @app.route('/upload')
 def upload():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('500.html'), 404

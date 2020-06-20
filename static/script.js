6f73476f633dd0e037d4d49520922fa69c9a4b19
Elizabeth Tackett <emctackett@gmail.com>
2020-06-20 08:44:12 -0500

    implement print functionality integration w/responsive button

--- static/script.js
+++ static/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
   $('#print-pdf').click((e) => {
     e.preventDefault();
+
+    let pdf = $('#pdf')[0];
+    pdf.focus();
+    pdf.contentWindow.print();
     console.log('printing');
-  })
+  });
 
   $('#download-pdf').click((e) => {
     e.preventDefault();
